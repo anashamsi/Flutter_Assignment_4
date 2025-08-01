8cbc69e74c8977bde1fb5877b4ea88eded7eeaed
anashamsi <anasshamsi500@gmail.com>
2025-08-01 02:23:28 -0700

    update login page

--- lib/Login_Page.dart
+++ lib/Login_Page.dart
@@ -9,75 +9,132 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          CircleAvatar(
-            backgroundColor: Colors.white,
-            radius: 60,
-            child: Image.asset('assets/images/logo-01.png'),
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Welcome!',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-          ),
-          SizedBox(height: 10),
-          Text('Please login or sign up to continue our app'),
-          SizedBox(height: 25),
-          Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
-          TextField(
-            decoration: InputDecoration(
-              isDense: true,
-              hint: Text('Enter your email'),
-            ),
-          ),
-          SizedBox(height: 10),
-          Text('Password', style: TextStyle(fontWeight: FontWeight.bold)),
-          TextField(
-            decoration: InputDecoration(
-              isDense: true,
-              hint: Text('Enter your password'),
-            ),
-          ),
-          SizedBox(height: 50),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(backgroundColor: Colors.black),
-            child: Text(
-              'Login',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 80,
+                child: Image.asset('assets/images/logo-01.png'),
               ),
             ),
-          ),
-          SizedBox(height: 5),
-          Text('or'),
-          SizedBox(height: 5),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(backgroundColor: Colors.white),
-            child: Text(
-              'Continue with Google',
+            SizedBox(height: 20),
+            Text(
+              'Welcome!',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
+                fontSize: 18,
               ),
             ),
-          ),
-          SizedBox(height: 5),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(backgroundColor: Colors.white),
-            child: Text(
-              'Continue with Apple',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+            SizedBox(height: 5),
+            Text(
+              'Please login or sign up to continue our app',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Email',
+                  style: TextStyle(fontWeight: FontWeight.bold, height: .5),
+                ),
+
+                TextField(
+                  decoration: InputDecoration(
+                    // hintText: 'Enter your email',
+                    isDense: true,
+                    suffixIcon: Icon(Icons.verified),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Password',
+                  style: TextStyle(fontWeight: FontWeight.bold, height: .5),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    // hintText: 'Enter your password',
+                    isDense: true,
+                    suffixIcon: Icon(Icons.verified),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 5),
+            Center(
+              child: Text('or', style: TextStyle(color: Colors.grey)),
+            ),
+            SizedBox(height: 5),
+            TextButton.icon(
+              icon: CircleAvatar(
+                radius: 15,
+                backgroundColor: Colors.white,
+                child: Image.asset(
+                  'assets/images/google-logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.white),
+              label: Text(
+                ' Continue with Google',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 5),
+            TextButton.icon(
+              icon: CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.white,
+                child: Image.asset(
+                  'assets/images/apple-logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.white),
+              label: Text(
+                ' Continue with Apple',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
