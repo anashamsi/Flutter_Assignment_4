1a3f2d52b4b611c75004e0ee731cd3e01a2b06dd
anashamsi <anasshamsi500@gmail.com>
2025-08-01 03:03:39 -0700

    login page ui complete

--- lib/Login_Page.dart
+++ lib/Login_Page.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 
+TextEditingController email_controller = TextEditingController();
+TextEditingController password_controller = TextEditingController();
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -11,129 +14,158 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
 
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 80,
-                child: Image.asset('assets/images/logo-01.png'),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome!',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(height: 5),
-            Text(
-              'Please login or sign up to continue our app',
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            ),
-            SizedBox(height: 40),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Email',
-                  style: TextStyle(fontWeight: FontWeight.bold, height: .5),
-                ),
-
-                TextField(
-                  decoration: InputDecoration(
-                    // hintText: 'Enter your email',
-                    isDense: true,
-                    suffixIcon: Icon(Icons.verified),
+                Center(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 80,
+                    child: Image.asset('assets/images/logo-01.png'),
                   ),
                 ),
-              ],
-            ),
-
-            SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+                SizedBox(height: 20),
                 Text(
-                  'Password',
-                  style: TextStyle(fontWeight: FontWeight.bold, height: .5),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    // hintText: 'Enter your password',
-                    isDense: true,
-                    suffixIcon: Icon(Icons.verified),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(backgroundColor: Colors.black),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Login',
+                  'Welcome!',
                   style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 18,
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 5),
-            Center(
-              child: Text('or', style: TextStyle(color: Colors.grey)),
-            ),
-            SizedBox(height: 5),
-            TextButton.icon(
-              icon: CircleAvatar(
-                radius: 15,
-                backgroundColor: Colors.white,
-                child: Image.asset(
-                  'assets/images/google-logo.png',
-                  fit: BoxFit.cover,
+                SizedBox(height: 5),
+                Text(
+                  'Please login or sign up to continue our app',
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
                 ),
-              ),
-              onPressed: () {},
-              style: TextButton.styleFrom(backgroundColor: Colors.white),
-              label: Text(
-                ' Continue with Google',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
+                SizedBox(height: 40),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(fontWeight: FontWeight.bold, height: .5),
+                    ),
 
-            SizedBox(height: 5),
-            TextButton.icon(
-              icon: CircleAvatar(
-                radius: 25,
-                backgroundColor: Colors.white,
-                child: Image.asset(
-                  'assets/images/apple-logo.png',
-                  fit: BoxFit.cover,
+                    TextField(
+                      textAlignVertical: TextAlignVertical.bottom,
+                      controller: email_controller,
+                      decoration: InputDecoration(
+                        // hintText: 'Enter your email',
+                        isDense: true,
+                        suffixIcon: Icon(Icons.verified),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              onPressed: () {},
-              style: TextButton.styleFrom(backgroundColor: Colors.white),
-              label: Text(
-                ' Continue with Apple',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+
+                SizedBox(height: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Password',
+                      style: TextStyle(fontWeight: FontWeight.bold, height: .5),
+                    ),
+                    TextField(
+                      textAlignVertical: TextAlignVertical.bottom,
+                      controller: password_controller,
+                      decoration: InputDecoration(
+                        // hintText: 'Enter your password',
+                        isDense: true,
+                        suffixIcon: Icon(Icons.verified),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+                SizedBox(height: 30),
+                TextButton(
+                  onPressed: () {
+                    var email = email_controller.text.toString();
+                    print(email);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    overlayColor: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.5),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Center(
+                  child: Text('or', style: TextStyle(color: Colors.grey)),
+                ),
+                SizedBox(height: 5),
+                TextButton.icon(
+                  icon: ClipOval(
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.transparent,
+                      child: Image.asset(
+                        'assets/images/google-logo.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+
+                  style: TextButton.styleFrom(
+                    overlayColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    elevation: 3,
+                    shadowColor: Colors.black,
+                  ),
+                  label: Text(
+                    'Continue with Google',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+                TextButton.icon(
+                  icon: ClipOval(
+                    child: CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.transparent,
+                      child: Image.asset(
+                        'assets/images/apple-logo.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    overlayColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    elevation: 3,
+                    shadowColor: Colors.black,
+                  ),
+                  label: Text(
+                    ' Continue with Apple',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
